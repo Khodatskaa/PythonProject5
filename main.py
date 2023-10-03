3521de39fd57b238f07436437812003edfe3b482
Khodatskaa <khodatskaalina@gmail.com>
2023-10-03 20:30:17 +0300

    Ex 3 was done

---
+++ main.py
@@ -0,0 +1,12 @@
+def find_min_of_four(a, b, c, d, e):
+    min_value = min(a, b, c, d, e)
+    return min_value
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+num3 = int(input('Enter third number: '))
+num4 = int(input('Enter fourth number: '))
+num5 = int(input('Enter fifth number: '))
+
+result = find_min_of_four(num1, num2, num3, num4, num5)
+print(f"The minimal number is {result}")
