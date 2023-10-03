0181ddf767ca2915612faab6f8a24eaa26c9ef76
Khodatskaa <khodatskaalina@gmail.com>
2023-10-03 20:22:56 +0300

    Ex 2 was done

---
+++ main.py
@@ -0,0 +1,9 @@
+def display_even_numbers(start, end):
+    for number in range(start, end + 1):
+        if number % 2 == 0:
+            print(number)
+
+start_number = int(input('Enter start number: '))
+end_number = int(input('Enter start number: '))
+
+display_even_numbers(start_number, end_number)
