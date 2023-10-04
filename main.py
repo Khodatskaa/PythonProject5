161f9ff2f98dff831c35d07f5d90c97df03373f7
Khodatskaa <khodatskaalina@gmail.com>
2023-10-04 11:28:47 +0300

    Ex 1 was done

---
+++ main.py
@@ -0,0 +1,9 @@
+def my_text():
+    return """
+    "Don't compare yourself with anyone in this world…
+if you do so, you are insulting yourself."
+Bill Gates
+    """
+
+if __name__ == '__main__':
+    print(my_text())
